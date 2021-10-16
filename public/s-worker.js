85455b8d4ee001db5236d8d813aea0cf47c8e2b9
unknown <rislammb@gmail.com>
2021-10-16 14:26:54 +0600

    another update for pwa

--- public/s-worker.js
+++ public/s-worker.js
@@ -1,15 +1,16 @@
-const staticCache = 'site-static-v1';
+const staticCache = 'site-static-v2';
 const dynamicCache = 'site-dynamic-v1';
 
 const fileToCache = [
   '/',
   '/index.html',
+  '/day',
+  '/contact',
   'https://fonts.googleapis.com/css?family=Titillium+Web&display=swap',
   'https://fonts.gstatic.com/s/titilliumweb/v10/NaPecZTIAOhVxoMyOr9n_E7fdMPmDQ.woff2',
 ];
 
 self.addEventListener('install', (evt) => {
-  console.log('install started');
   evt.waitUntil(
     caches.open(staticCache).then((cache) => {
       return cache.addAll(fileToCache);
@@ -18,7 +19,6 @@ self.addEventListener('install', (evt) => {
 });
 
 self.addEventListener('activate', (evt) => {
-  console.log('activate started');
   evt.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
@@ -31,26 +31,34 @@ self.addEventListener('activate', (evt) => {
 });
 
 self.addEventListener('fetch', (evt) => {
-  console.log('fetch started');
   if (!(evt.request.url.indexOf('http') === 0)) {
     console.log('if', evt.request);
     return;
   }
   evt.respondWith(
-    caches.match(evt.request).then((cacheRes) => {
-      console.log('evt.request', evt.request);
-      return (
-        cacheRes ||
-        fetch(evt.request).then((fetchRes) => {
-          console.log('fetch res', fetchRes);
-          return caches.open(dynamicCache).then((cache) => {
-            cache.put(evt.request.url, fetchRes.clone());
-            // check cached items size
-            // limitCacheSize(dynamicCache, 15);
-            return fetchRes;
-          });
-        })
-      );
-    })
+    caches
+      .match(evt.request)
+      .then((cacheRes) => {
+        console.log('cache res', cacheRes);
+        return (
+          cacheRes ||
+          fetch(evt.request).then((fetchRes) => {
+            console.log('fetch res', fetchRes);
+            return caches.open(dynamicCache).then((cache) => {
+              cache.put(evt.request.url, fetchRes.clone());
+              // check cached items size
+              // limitCacheSize(dynamicCache, 15);
+              return fetchRes;
+            });
+          })
+        );
+      })
+      .catch(() => {
+        if (evt.request.url.indexOf('/day') > -1) {
+          return caches.match('/day');
+        } else {
+          return caches.match('/');
+        }
+      })
   );
 });
